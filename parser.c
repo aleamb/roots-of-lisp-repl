316d5f951302cfcf7736e53d1cc4cbc449781ac5
aleamb <jandroz@gmail.com>
2020-01-13 00:44:29 +0100

    Add quote character to BNF

--- parser.c
+++ parser.c
@@ -4,14 +4,18 @@
     Parser
 
     BNF from http://cuiwww.unige.ch/isi/bnf/LISP/BNFlisp.html
+    modified to suuport quote character (')
 
     s_expression = atomic_symbol \
+                / quoted \
                 / "(" s_expression "."s_expression ")" \
                 / list 
 
     list = "(" s_expression < s_expression > ")"
 
     atomic_symbol = letter atom_part
+
+    quoted = "'" atomic_symbol / list
 
     atom_part = empty / letter atom_part / number atom_part
 
@@ -28,5 +32,7 @@
 void s_expression(BUFFER* buffer) {
 
     int ch = peek_char(buffer);
+
+
 
 }
