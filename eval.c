64526e0c47d083190f8359f19af67263c57d5575
Alejandro Ambroa <aambroa@bloquetech.com>
2020-03-09 15:49:49 +0100

    Finished basic operators

--- eval.c
+++ eval.c
@@ -1,9 +1,11 @@
 #include <stdarg.h>
+#include <string.h>
 #include "eval.h"
 
 S_EXP* T;
 S_EXP* NIL;
 
+S_EXP* eval(S_EXP*, S_EXP*);
 
 S_EXP* quote(S_EXP* s_expr) {
     return s_expr;
@@ -17,7 +19,7 @@ S_EXP* atom(S_EXP* s_expr) {
 }
 
 S_EXP* eq(S_EXP* a, S_EXP* b) {
-  return (a->type == ATOM && b->type == ATOM && strcmpi(rol_get_atom_name(a), rol_get_atom_name(b)) == 0) || (rol_nil(a) && ro_nil(b)) ? T : NIL;
+  return (a->type == ATOM && b->type == ATOM && strcmpi(rol_get_atom_name(a), rol_get_atom_name(b)) == 0) || (rol_nil(a) && rol_nil(b)) ? T : NIL;
 }
 
 S_EXP* car(S_EXP* expr) { 
@@ -32,12 +34,26 @@ S_EXP* cons(S_EXP* expr1, S_EXP* expr2) {
   return rol_make_cons(expr1, expr2);
 }
 
-S_EXP* cond(S_EXP* expr1, ...) { 
+S_EXP* cond(S_EXP*  env, S_EXP* expr1, ...) { 
   va_list argp;
   va_start(argp, expr1);
-
-  S_EXP* expr = va_arg(argp, S_EXP*);
-
-  
+  S_EXP* expr = NULL;
+  while ((expr = va_arg(argp, S_EXP*)) != NULL) {
+    S_EXP* car_result = eval(rol_get_car(expr), env);
+    if (!rol_nil(car_result)) {
+      return eval(rol_get_cdr(expr), env);
+    }
+  }
   va_end(argp);
+  return NIL;
+}
+
+//
+
+S_EXP* nullp(S_EXP *exp) {
+
+}
+
+S_EXP* eval(S_EXP* input, S_EXP* env) {
+
 }
